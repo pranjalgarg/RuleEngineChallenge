a5b26d20a88caa577cab7b0327dcf2bfc3e2abdd
pranjalgarg <pranjal.g1005@gmail.com>
2019-08-04 23:40:17 +0530

    RuleEngineChallenge

---
+++ RuleEngine/Comparison.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuleEngine
+{
+    class Comparison
+    {
+        internal List<object> DoComparison(List<List<object>> jsonData, List<List<object>> ruleData)
+        {
+            List<object> resultValue = new List<object>();
+            try
+            {
+                for (int i = 0; i < jsonData.Count; i++)
+                {
+                    for (int j = 1; j < ruleData.Count; j++)
+                    {
+                        if ((Equals(jsonData[i][0], ruleData[j][0])) && (Equals(jsonData[i][1], ruleData[j][1])))
+                        {
+                            string comparisonTypeValue = Convert.ToString(ruleData[j][3]).ToLowerInvariant(); ;
+                            switch (comparisonTypeValue)
+                            {
+                                case "greaterthan":
+                                    string valueType = Convert.ToString(ruleData[j][1]);
+                                    var greaterthanComparison = false;
+                                    if (valueType == "Datetime")
+                                    {
+                                        int value = DateTime.Compare(Convert.ToDateTime(jsonData[i][2]), Convert.ToDateTime(ruleData[j][2]));
+                                        if (value > 0)
+                                        {
+                                            greaterthanComparison = true;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        greaterthanComparison = Convert.ToInt32(jsonData[i][2]) > Convert.ToInt32(ruleData[j][2]);
+                                    }
+                                    if (greaterthanComparison)
+                                    {
+                                        resultValue.Add(jsonData[i][0]);
+                                    }
+                                    break;
+
+                                case "equal":
+                                    var equalComparison = Equals(jsonData[i][2], ruleData[j][2]);
+                                    if (equalComparison)
+                                    {
+                                        resultValue.Add(jsonData[i][0]);
+                                    }
+
+                                    break;
+                                case "lessthan":
+                                    string Type = Convert.ToString(ruleData[j][1]);
+                                    var lessthanComparison = false;
+                                    if (Type == "Datetime")
+                                    {
+                                        int value = DateTime.Compare(Convert.ToDateTime(jsonData[i][2]), Convert.ToDateTime(ruleData[j][2]));
+                                        if (value > 0)
+                                        {
+                                            lessthanComparison = true;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        lessthanComparison = Convert.ToInt32(jsonData[i][2]) > Convert.ToInt32(ruleData[j][2]);
+                                    }
+                                    if (lessthanComparison)
+                                    {
+                                        resultValue.Add(jsonData[i][0]);
+                                    }
+                                    break;
+                                case "greaterthanequal":
+                                    string typeValue = Convert.ToString(ruleData[j][1]);
+                                    var greaterthanEqualComparison = false;
+                                    if (typeValue == "Datetime")
+                                    {
+                                        int value = DateTime.Compare(Convert.ToDateTime(jsonData[i][2]), Convert.ToDateTime(ruleData[j][2]));
+                                        if (value >= 0)
+                                        {
+                                            greaterthanEqualComparison = true;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        greaterthanEqualComparison = Convert.ToInt32(ruleData[i][2]) >= Convert.ToInt32(ruleData[j][2]);
+                                    }
+                                    if (greaterthanEqualComparison)
+                                    {
+                                        resultValue.Add(jsonData[i][0]);
+                                    }
+                                    break;
+                                case "lessthanequal":
+                                    string dataTypeValue = Convert.ToString(ruleData[j][1]);
+                                    var lessthanEqualComparison = false;
+                                    if (dataTypeValue == "Datetime")
+                                    {
+                                        int value = DateTime.Compare(Convert.ToDateTime(jsonData[i][2]), Convert.ToDateTime(ruleData[j][2]));
+                                        if (value >= 0)
+                                        {
+                                            lessthanEqualComparison = true;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        lessthanEqualComparison = Convert.ToInt32(jsonData[i][2]) >= Convert.ToInt32(ruleData[j][2]);
+                                    }
+                                    if (lessthanEqualComparison)
+                                    {
+                                        resultValue.Add(jsonData[i][0]);
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return resultValue;
+        }
+    }
+}
---
+++ RuleEngine/RuleEngineImplementation.cs
@@ -0,0 +1,175 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RuleEngine
+{
+    class RuleEngineImplementation
+    {
+        private List<List<object>> ReadJsonFile(string path)
+        {
+            List<List<object>> jsonDataList = new List<List<object>>();
+            try
+            {
+                string jsonFilePath = path;
+                StreamReader jsonDataStreamReader = new StreamReader(jsonFilePath);
+                string readJsonData = jsonDataStreamReader.ReadToEnd();
+                List<DataUnit> jsonItems = JsonConvert.DeserializeObject<List<DataUnit>>(readJsonData);
+                jsonDataList = getJsonData(jsonItems);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return jsonDataList;
+        }
+        private List<List<object>> ReadRule(string path)
+        {
+            List<List<object>> ruleBookList = new List<List<object>>();
+            try
+            {
+                string rulePath = path;
+                StreamReader ruleDataStreamReader = new StreamReader(rulePath);
+
+                while (!ruleDataStreamReader.EndOfStream)
+                {
+                    var ruleLine = ruleDataStreamReader.ReadLine();
+                    string[] ruleLineData = ruleLine.Split(',');
+                    List<string> list = ruleLineData.ToList();
+                    List<object> ruleDataList = GetRuleData(list);
+                    ruleBookList.Add(ruleDataList);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+            }
+            return ruleBookList;
+        }
+
+
+
+        private List<List<Object>> getJsonData(List<DataUnit> Data)
+        {
+           List<List<object>> jsonValuesList = new List<List<object>>();
+            try
+            {
+                List<object> jsonValues = new List<object>();
+                for (int i = 0; i < Data.Count; i++)
+                {
+                    object[] values = new object[3];
+                    int j = 0;
+                    values[j] = Data[i].signal;
+                    values[++j] = Data[i].value_type;
+                    string dataType = Data[i].value_type.ToLowerInvariant();
+                    switch (dataType)
+                    {
+                        case "integer":
+                            if (double.TryParse(Data[i].value, out double resultInteger)) { values[++j] = resultInteger; }
+                            else { values[++j] = Data[i].value; }
+                            break;
+
+                        case "datetime":
+                            if (DateTime.TryParse(Data[i].value, out DateTime resultDateTime)) { values[++j] = resultDateTime; }
+                            else { values[++j] = Data[i].value; }
+                            break;
+
+                        default:
+                            values[++j] = Data[i].value;
+                            break;
+                    }
+                    jsonValues = values.ToList();
+                    jsonValuesList.Add(jsonValues);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return jsonValuesList;
+        }
+
+        private  List<object> GetRuleData(List<string> list)
+        {
+            List<object> ruleValuesList = new List<object>();
+            try
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    List<object> values = new List<object>();
+                    int j = 0;
+                    values.Add(list[i]);
+                    values.Add(list[++i]);
+                    string dataType = list[i].ToLowerInvariant();
+                    switch (dataType)
+                    {
+                        case "integer":
+                            if (double.TryParse(list[++i], out double resultInteger))
+                            {
+                                values.Add(resultInteger);
+                            }
+                            else
+                            {
+                                values.Add(list[++i]);
+                            }
+                            break;
+
+                        case "datetime":
+                            if (DateTime.TryParse(list[++i], out DateTime resultDateTime))
+                            {
+                                values.Add(resultDateTime);
+                            }
+                            else
+                            {
+                                values.Add(list[++i]);
+                            }
+                            break;
+
+                        default:
+                            values.Add(list[++i]);
+                            break;
+                    }
+                    values.Add(list[++i]);
+                    ruleValuesList = values;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return ruleValuesList;
+        }
+
+        private void printVoilatedSignalData(List<object>resultValue)
+        {
+            if (resultValue.Count > 0)
+            {
+                Console.WriteLine("The signal that voilates the rule");
+                foreach (var value in resultValue)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+            Console.ReadLine();
+        }
+
+
+        public static void Main(string[] args)
+        {
+          RuleEngineImplementation ruleEngine = new RuleEngineImplementation();
+          var jsonData =  ruleEngine.ReadJsonFile("raw_data.json");
+          var ruleData= ruleEngine.ReadRule("RuleBook.csv");
+          Comparison comparison = new Comparison();
+          var resultValue = comparison.DoComparison(jsonData, ruleData);
+          ruleEngine.printVoilatedSignalData(resultValue);
+        }
+    }
+ }
+
+
+ 
+
